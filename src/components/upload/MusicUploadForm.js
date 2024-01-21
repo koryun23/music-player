1919e2e30e0a735e7aeebe02a56f2a46c76d9162
Koryun Grigoryan <koryun.grigoryan.5@gmail.com>
2024-01-22 02:16:25 +0400

    adding error messages in the MusicUploadForm

--- src/components/upload/MusicUploadForm.js
+++ src/components/upload/MusicUploadForm.js
@@ -9,6 +9,7 @@ export default function MusicUploadForm(props) {
     const [fileName, setFileName] = useState("");
     const [songName, setSongName] = useState("");
     const [artistName, setArtistName] = useState("");
+    const [errorMessages, setErrorMessages] = useState([]);
 
     const changeSongName = (event) => {
         setSongName(event.target.value);
@@ -18,16 +19,31 @@ export default function MusicUploadForm(props) {
         setArtistName(event.target.value);
     }
 
+    const changeSelectedFile = (event) => {
+        let enteredFileName = event.target.value;
+        enteredFileName = enteredFileName.slice(enteredFileName.lastIndexOf("\\") + 1);
+        let format = enteredFileName.slice(enteredFileName.lastIndexOf("."));
+        if(format !== ".mp3" && format !== ".wav") {
+            setErrorMessages([`Unsupported file type(${format}): only .mp3 and .wav are supported.`]);
+        }
+        setFileName(enteredFileName);
+        setErrorMessages([]);
+    }
+
     return (
         <form className="music-upload-form">
             <div className="form form-group">
+                {
+                    errorMessages.length > 0 && <small className="error">{errorMessages[0]}</small>
+                }
                 <input type="text" 
                        name="fileName" 
                        placeholder="No File Selected" 
                        className="file-name" 
                        value={fileName}
                        readOnly />
-                <small className="form-text text-muted">Supported file formats: .mp3, .wav</small>
+                <small>Supported file formats: .mp3, .wav</small>
+
             </div>
             <div className="form form-group">
                 <input type="text" 
@@ -46,10 +62,10 @@ export default function MusicUploadForm(props) {
                        value={artistName}/>
             </div>
             <div className="form form-group">
-                <input type="submit"
+                <input type="file"
                        name="submit"
                        className="btn btn-submit" 
-                       value="Choose File"/>
+                       onChange={changeSelectedFile}/>
                 <MusicUploadButton />
             </div>
         </form>
